8faf655d8f2ebd4f889f03b1c2ebe79a421a1a29
Chiaya T <709210chiaya@gmail.com>
2023-09-08 17:31:18 -0600

    added actor, stub dao methods

--- src/main/java/com/skilldistillery/film/dao/FilmDAO.java
+++ src/main/java/com/skilldistillery/film/dao/FilmDAO.java
@@ -1,5 +1,21 @@
 package com.skilldistillery.film.dao;
 
-public interface FilmDAO {
+import java.sql.SQLException;
+import java.util.List;
 
+import com.skilldistillery.film.entities.Actor;
+import com.skilldistillery.film.entities.Film;
+
+public interface FilmDAO {
+	  public Film findFilmById(int filmId)throws SQLException;
+	  public List<Actor> findActorsByFilmId(int filmId);
+
+		public Actor findActorById(int actorId)throws SQLException;
+		public Actor createActor(Actor actor);
+		public Film createFilm(Film film);
+		public boolean deleteActor(Actor actor);
+		public boolean deleteFilm(Film film);
+		public boolean saveActor(Actor actor);
+		public String getLanguage(int langNumber) throws SQLException;
+		public List<Film> findFilmWithKeyword(String keyword) throws SQLException;
 }
