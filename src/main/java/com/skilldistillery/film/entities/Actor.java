d5a7ce57bd423c3fb8223f5cb21831b48dcf2a40
Cody Comstock <mr.codysherwood@hotmail.com>
2023-09-08 19:19:50 -0500

    updated film and actor

--- src/main/java/com/skilldistillery/film/entities/Actor.java
+++ src/main/java/com/skilldistillery/film/entities/Actor.java
@@ -1,5 +1,92 @@
 package com.skilldistillery.film.entities;
 
-public class Actor {
+import java.util.List;
+import java.util.Objects;
 
+import com.skilldistillery.filmquery.entities.Actor;
+import com.skilldistillery.filmquery.entities.Film;
+
+public class Actor {
+	private int id;
+	private String firstName;
+	private String lastName;
+	private List<Film> films;
+
+	public Actor() {
+
+	}
+
+	public Actor(int id, String firstName, String lastName) {
+		super();
+		this.id = id;
+		this.firstName = firstName;
+		this.lastName = lastName;
+	}
+
+	public Actor(String firstName, String lastName) {
+		super();
+		this.firstName = firstName;
+		this.lastName = lastName;
+	}
+
+	public List<Film> getFilms() {
+		return films;
+	}
+
+	public void setFilms(List<Film> films) {
+		this.films = films;
+	}
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public String getFirstName() {
+		return firstName;
+	}
+
+	public void setFirstName(String firstName) {
+		this.firstName = firstName;
+	}
+
+	public String getLastName() {
+		return lastName;
+	}
+
+	public void setLastName(String lastName) {
+		this.lastName = lastName;
+	}
+
+	@Override
+	public String toString() {
+		StringBuilder builder = new StringBuilder();
+		builder.append("Actor ID= ");
+		builder.append(id);
+		builder.append(", First Name= ");
+		builder.append(firstName);
+		builder.append(", Last Name= ");
+		builder.append(lastName);
+		return builder.toString();
+	}
+
+	@Override
+	public int hashCode() {
+		return Objects.hash(id);
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Actor other = (Actor) obj;
+		return id == other.id;
+	}
 }
